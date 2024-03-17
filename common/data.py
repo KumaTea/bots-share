a035f0efc25aec77485be28a401b207ae1edef06
KumaTea <KumaTea@outlook.com>
2024-03-17 21:18:52 +0800

    add shared libs

---
+++ common/data.py
@@ -0,0 +1,11 @@
+url_regex = r'https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|' \
+            r'www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|' \
+            r'https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|' \
+            r'www\.[a-zA-Z0-9]+\.[^\s]{2,}'
+
+USER_AGENT = (
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/123.0.0.0 '
+    'Safari/537.36'
+)
