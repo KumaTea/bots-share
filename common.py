8b14bdaa99f3b7ad492c381239210f2323131bb2
KumaTea <KumaTea@outlook.com>
2026-01-13 02:13:56 +0800

    add no_quote option

--- common.py
+++ common.py
@@ -10,5 +10,18 @@ is_old_pyrogram = pyrogram_version <= (2, 0, 106)
 if is_old_pyrogram:
     no_preview = {'disable_web_page_preview': True}
 else:
+    # from pyrogram.types import LinkPreviewOptions
     from pyrogram.types.messages_and_media.link_preview_options import LinkPreviewOptions
     no_preview = {'link_preview_options': LinkPreviewOptions(is_disabled=True)}
+
+
+# old `quote` parameter is deprecated
+# because Telegram has assigned "quote" as a new feature
+if is_old_pyrogram:
+    no_quote = {'quote': False}
+    do_quote = {'quote': True}
+else:
+    # from pyrogram.types import ReplyParameters
+    from pyrogram.types.messages_and_media.reply_parameters import ReplyParameters
+    no_quote = {'reply_parameters': ReplyParameters(message_id=None)}
+    do_quote = {}  # default behavior is to reply
